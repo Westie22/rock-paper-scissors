21545b8e0b9018c6ac80524caddbe01a32cffdb6
Westie22 <jinyoungwest@gmail.com>
2022-12-03 14:07:47 +0000

    create script for my choice of 3 options and randomley choice for computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,21 @@ let choices = ["rock", "paper", "scissors"];
 window.onload = function() {
     for (let i = 0; i < 3; i++) {
         // create <img> tag
-        // <img id ="rock" scr="rock.png"
+        // <img id ="rock" scr="rock.png">
         let choice = document.createElement("img");
         choice.id = choices[i];
-        choices.scr = choices[i] + ".png";
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
         document.getElementById("choices").append(choice);
     }
 
+}
+// My choice of rock, papter, scissors
+function selectChoice() {
+    me = this.id;
+    document.getElementById("my-choice").src = me + ".png";
+
+    // randome choices for computer
+    computer = choices[Math.floor(Math.random() * 3)]; 
+    document.getElementById("computer-choice").src = computer + ".png";
 }
